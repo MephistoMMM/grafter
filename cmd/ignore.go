33af20f18f2ad2567f4cc01a62e778d1d0cff19a
Mephis Pheies <mephistommm@gmail.com>
2018-12-09 22:21:23 +0800

    write description for ignore command

--- cmd/ignore.go
+++ cmd/ignore.go
@@ -21,24 +21,15 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // ignoreCmd represents the ignore command
 var ignoreCmd = &cobra.Command{
 	Use:   "ignore",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ignore called")
-	},
+	Short: "Manage ignore configurations of missions",
+	Long:  `Ignore command provides some subcommands to manage the value of ignore field of mission's configuration, including add, remove and list ignore regexps.`,
+	Args:  cobra.NoArgs,
 }
 
 func init() {
